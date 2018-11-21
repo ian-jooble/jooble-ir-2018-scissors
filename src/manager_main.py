06d8486283c37bb6cbdc223f33be8cca66b6464d
Viktor Overko <vitalinsh.overko@gmail.com>
2018-11-21 23:30:08 +0200

    read dataset, saving and loading index

---
+++ src/manager_main.py
@@ -0,0 +1,114 @@
+import json
+import gzip
+import os
+
+import requests
+import pandas as pd
+
+indexer_url = "http://127.0.0.1:13500/"
+text_processing_url = "http://127.0.0.1:13501/"
+ranking_url = "http://127.0.0.1:13502/"
+snippets_url = "http://127.0.0.1:13503/"
+res_page_form_url = "http://127.0.0.1:13504/"
+
+
+def test_system():
+    r = requests.post(indexer_url)
+    print(r.status_code)
+    print(r.text)
+
+    #  normalize the document
+    r = requests.post(text_processing_url + "normalize_document",
+                      json={"id": 7, "title": "Садоводство",
+                            "text": "Саженцы декоративных и плодовых культур. "
+                                    "Могилев. Гарантия."})
+    print(r.status_code)
+    print(json.loads(r.text))
+    document = json.loads(r.text, encoding="utf-8")
+
+    #  add the document to index
+    r = requests.post(indexer_url + "indexer", json=document)
+    print(r.status_code)
+    print(r.text)
+
+    #  normalize the search query
+    r = requests.post(text_processing_url + "normalize_query",
+                      json="плодовые культуры")
+    print(r.status_code)
+    print(str(r.text))
+    search_query = r.text
+
+    # search in index
+    r = requests.post(indexer_url + "search",
+                      json=search_query)
+    print(r.status_code)
+    print(r.text)
+
+    # search in index
+    r = requests.post(indexer_url + "search",
+                      json="водител")
+    print(r.status_code)
+    print(r.text)
+    print(json.loads(r.text, encoding="utf-8"))
+
+
+def load_data(data_path):
+    docs_info = []
+    docs_text = []
+    file_names = os.listdir(data_path)
+
+    for file in file_names:
+        if "text" in file:
+            with gzip.open(os.path.join(data_path, file), "rb") as f:
+                for line in f:
+                    vacancy = json.loads(line)
+                    docs_text.append(vacancy)
+        else:
+            with gzip.open(os.path.join(data_path, file), "rb") as inf:
+                for line in inf:
+                    vacancy = json.loads(line)
+                    docs_info.append(vacancy)
+
+    assert len(docs_info) == len(docs_text)
+    assert "id" in docs_info[0].keys()
+    assert "id_job" in docs_text[0].keys()
+
+    docs_info = pd.DataFrame(docs_info)
+    docs_text = pd.DataFrame(docs_text)
+    docs_info.drop_duplicates(["id"], inplace=True)
+    docs_text.drop_duplicates(["id_job"], inplace=True)
+    data = docs_info.merge(docs_text, left_on='id', right_on='id_job', how='outer')
+    return data
+
+
+def add_dataset_to_index(dataset):
+    for index, row in dataset.iterrows():
+        r = requests.post(text_processing_url + "normalize_document", json=row.to_dict())
+        status_normilizer = r.status_code
+        doc_normilized = json.loads(r.text)
+        r = requests.post(indexer_url + "indexer", json=doc_normilized)
+        status_indexer = r.status_code
+
+        if status_indexer != 200 or status_normilizer != 200:
+            print(" Request number", index, ": ")
+            print("error")
+    print("dataset adding finished!")
+
+
+# dataset_path = os.path.join("..", "data", "by_jobs", "by")
+# documents = load_data(dataset_path)
+# print("Number of documents =", len(documents))
+# print("Text of first five documents:")
+# print(documents.head()["text"])
+#
+# add_dataset_to_index(documents.loc[0:1000])
+# r = requests.post(indexer_url + "save_index")
+
+#  search in index
+# r = requests.post(indexer_url + "search", json="вектор")
+# print(r.status_code)
+# print(r.text)
+# print(json.loads(r.text, encoding="utf-8"))
+
+# -------##--------##--------##--------##--------##--------##--------##--------#
+
--- src/server_text_processing.py
+++ src/server_text_processing.py
@@ -3,9 +3,69 @@ import json
 from flask import Flask, request
 import nltk
 from nltk.stem.snowball import RussianStemmer
+from nltk.corpus import stopwords
+from nltk import wordpunct_tokenize
 
 nltk.download('punkt')
+nltk.download('stopwords')
 app = Flask(__name__)
+
+
+def _calculate_languages_ratios(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return a dictionary that looks like {'french': 2, 'spanish': 4, 'english': 0}
+
+    @param text: Text whose language want to be detected
+    @type text: str
+
+    @return: Dictionary with languages and unique stopwords seen in analyzed text
+    @rtype: dict
+    """
+    languages_ratios = {}
+    words = wordpunct_tokenize(text.lower())
+
+    # Compute per language included in nltk number of unique stopwords appearing in analyzed text
+    for language in stopwords.fileids():
+        stopwords_set = set(stopwords.words(language))
+        words_set = set(words)
+        common_elements = words_set.intersection(stopwords_set)
+        languages_ratios[language] = len(common_elements)  # language "score"
+    return languages_ratios
+
+
+def detect_language(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return the highest scored.
+
+    It uses a stopwords based approach, counting how many unique stopwords
+    are seen in analyzed text.
+
+    @param text: Text whose language want to be detected
+    @type text: str
+
+    @return: Most scored language guessed
+    @rtype: str
+    """
+    ratios = _calculate_languages_ratios(text)
+    most_rated_language = max(ratios, key=ratios.get)
+    return most_rated_language
+
+
+def normalize_text(text):
+    """Preprocess text data"""
+    # Stemming
+    rus_stemmer = RussianStemmer()
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    text = [rus_stemmer.stem(word) for word in text]
+    # Excluding Stop-words
+    text = [word for word in text if
+            word not in stopwords.words('english') and word.isalpha()]
+    text = " ".join(text)
+
+    return text
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,32 +77,27 @@ def index():
 def normalize_document():
     document = request.json
     field_for_norm = ["text", "title"]
-    rus_stemmer = RussianStemmer()
 
     for field in field_for_norm:
-        text = document[field].lower()
-        text = nltk.word_tokenize(text)
-        text = [rus_stemmer.stem(word) for word in text]
-        text = " ".join(text)
+        text = document[field]
+        text = normalize_text(text)
         document[field + "_normalized"] = text
 
     return json.dumps(document, ensure_ascii=False)
 
 
 @app.route("/normalize_query", methods=["POST"])
-def normalize_string():
-    query = request.json
-    rus_stemmer = RussianStemmer()
-    text = query.lower()
-    text = nltk.word_tokenize(text)
-    text = [rus_stemmer.stem(word) for word in text]
-    text = " ".join(text)
+def normalize_query():
+    text = request.json
+    text = normalize_text(text)
     return text
 
 
 @app.route("/detect_language", methods=["POST"])
 def det_lang():
-    return "detected language"
+    text = request.json
+    language = detect_language(text)
+    return language
 
 
 if __name__ == "__main__":
